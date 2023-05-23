3227fba1921896508d2e1d444d48bc30dc074b41
Shimon <shimonkocor@gmail.com>
2023-05-23 11:25:59 +0100

    Added url link to the news emails

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 
+from send_email import send_email
+
 api_key = "pub_22601ab85ce30946048f7320360f8399787b4"
 url = "https://newsdata.io/api/1/news?apikey=" \
-      "pub_22601ab85ce30946048f7320360f8399787b4&q=stock%20market%20us "
+      "pub_22601ab85ce30946048f7320360f8399787b4&q=stock%20market%20us&language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,7 +16,9 @@ result = request.json()
 body = ""
 for article in result["results"]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" \
+               + article["description"] + \
+               "\n" + article["link"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
