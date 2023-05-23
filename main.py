ee2006b115d81bf83307876759ea4843c3116317
Shimon <shimonkocor@gmail.com>
2023-05-23 11:06:58 +0100

    Access news titles and descriptions

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ api_key = "pub_22601ab85ce30946048f7320360f8399787b4"
 url = "https://newsdata.io/api/1/news?apikey=" \
       "pub_22601ab85ce30946048f7320360f8399787b4&q=stock%20market%20us "
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+result = request.json()
+
+# Access article titles and descriptions
+for article in result["results"]:
+      print(article["title"])
+      print(article["description"])
